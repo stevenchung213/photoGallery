43c944349946694be7cb8a713244197000d2108f
Caitlin Detro <caitlindetro@gmail.com>
2018-10-31 16:55:00 -0500

    Added currentPropertyId to be used for pulling photos for given property

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,16 +6,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentPropertyId: 0,
       photos: []
     };
-
+    
+    this.getRandomId = this.getRandomId.bind(this);
+    this.getProperty = this.getProperty.bind(this);
     this.getPhotos = this.getPhotos.bind(this);
   }
 
+  getRandomId(min, max) {
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getProperty() {
+    var propertyId = this.getRandomId(1, 100);
+    this.setState({currentPropertyId: propertyId});
+  }
+  
   getPhotos() {
     $.ajax({
       method: 'GET',
-      url: '/api/photogalleries',
+      url: '/api/photogalleries/' + this.props.currentPropertyId,
       success: (res) => {
         this.setState({photos: res})
       },
