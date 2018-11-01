b769192036c38af6d7100f80692eb9fd221dcb62
Caitlin Detro <caitlindetro@gmail.com>
2018-11-01 13:51:53 -0500

    Updated const in seed db and style guide to Hack Reactor

--- db/seed.js
+++ db/seed.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const Photo = require('./index.js');
 
-var getRandomId = function(min, max) {
-  var min = Math.ceil(min);
-  var max = Math.floor(max);
+const getRandomId = function(min, max) {
+  let min = Math.ceil(min);
+  let max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var getPhotoData = function(num) {
-  var data = [];
+const getPhotoData = function(num) {
+  let data = [];
 
   for (var i = 1; i <= num; i++) {
-    var photo = {
+    let photo = {
       _id: i,  
       propertyId: getRandomId(1, 100),
       photoURL: `https://s3-us-west-2.amazonaws.com/fec1-photo-gallery/P${i}.jpg`
@@ -22,6 +22,6 @@ var getPhotoData = function(num) {
   return data;
 }
 
-var photoData = getPhotoData(1045);
+const photoData = getPhotoData(1045);
 
 Photo.create(photoData);
